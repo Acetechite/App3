a8b764ff10de9159bc457df0f48ac4f80bd0ffaa
Acetechite <31084818+Acetechite@users.noreply.github.com>
2017-11-30 16:29:37 -0500

    Actual Maze Built
    I actually stopped being lazy and realted all prefabs to zero. Also the butterfly works and is physics based so good collisions and movement

---
+++ App3/Assets/Scripts/ButterflyController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButterflyController : MonoBehaviour {
+
+    public float thrust;
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        
+        float x = Input.GetAxis("Horizontal")*Time.deltaTime* 3.0f;
+        float z = Input.GetAxis("Vertical") * Time.deltaTime * 100.0f;
+
+        rb.velocity = transform.forward*-z;
+        rb.AddRelativeTorque(0,x,0);
+        
+    }
+}
