7466dbbd54e4689c62f5a2f9ba205ab313e9c5ce
Acetechite <31084818+Acetechite@users.noreply.github.com>
2017-11-30 17:12:09 -0500

    still working

--- App3/Assets/Scripts/ButterflyController.cs
+++ App3/Assets/Scripts/ButterflyController.cs
@@ -6,6 +6,7 @@ public class ButterflyController : MonoBehaviour {
 
     public float thrust;
     public Rigidbody rb;
+    public GameObject weapon;
 
 	// Use this for initialization
 	void Start () {
@@ -21,5 +22,15 @@ public class ButterflyController : MonoBehaviour {
         rb.velocity = transform.forward*-z;
         rb.AddRelativeTorque(0,x,0);
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Gun")
+        {
+            Destroy(other.gameObject);
+            var pistol = Instantiate(weapon,gameObject.transform);
+            pistol.transform.position = gameObject.transform.localPosition;
+        }
     }
 }
